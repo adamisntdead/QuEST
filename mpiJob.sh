7c1295c695eddfd5bb6b1dfc73d2dcf62bf9a132
ania.brown <ania.brown@gmail.com>
2017-05-12 16:07:07 +0100

    changed nTrials to 5, added enable-arcus-b to mpiJob

--- mpiJob.sh
+++ mpiJob.sh
@@ -2,14 +2,14 @@
 
 # set the number of nodes and processes per node. We are running one process on a single node
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks-per-node=8
 
 ##SBATCH --mem=50Gb
 # uncomment if NUM_QUBITS - log2(NUM_NODES) > 30
 #SBATCH --mem=100Gb
 
 # set max wallclock time
-#SBATCH --time=01:00:00
+#SBATCH --time=02:00:00
 
 # set name of job
 #SBATCH --job-name QUEST
@@ -20,9 +20,11 @@
 
 NUM_QUBITS=31
 EXE=demo
-export OMP_NUM_THREADS=16
+export OMP_NUM_THREADS=2
 
 module purge
 module load mvapich2
+
+. enable_arcus-b_mpi.sh
 
 mpirun $MPI_HOSTS ./$EXE $NUM_QUBITS
